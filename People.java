4de0300d2a633e93e4852d18cca530142830e4e8
Eris Suther <100593472+nest399@users.noreply.github.com>
2023-02-17 12:31:44 -0500

    Create People.java

---
+++ People.java
@@ -0,0 +1,203 @@
+/**
+ * Eris Suther
+ * Feb 15, 2023
+ */
+package dndProject;
+
+import java.util.List;
+
+public class People {
+	// character info (name, description, stats, health)
+	private String name;
+	private String description;
+	private double attack;
+	private double defense;
+	private double perception;
+	private double stealth;
+	private double totalHealth;
+	private double currentHealth;
+
+	// what items do they have
+	private List<Item> bag;
+
+	// what is their weapon
+	private Item weapon;
+
+	// description can change as injured
+
+	public People() {
+		setName("???");
+		setDescription(null);
+		setAttack(5);
+		setDefense(5);
+		setPerception(5);
+		setStealth(5);
+		setTotalHealth(25);
+		setCurrentHealth(25);
+	}
+
+	public People(String name, double attack, double defense, double perception, double stealth, double totalHealth) {
+		this.setName(name);
+		this.setDescription(null);
+		this.setAttack(attack);
+		this.setDefense(defense);
+		this.setPerception(perception);
+		this.setStealth(stealth);
+		this.setTotalHealth(totalHealth);
+		this.setCurrentHealth(totalHealth);
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the attack
+	 */
+	public double getAttack() {
+		return attack;
+	}
+
+	/**
+	 * @param attack the attack to set
+	 */
+	public void setAttack(double attack) {
+		this.attack = attack;
+	}
+
+	/**
+	 * @return the defense
+	 */
+	public double getDefense() {
+		return defense;
+	}
+
+	/**
+	 * @param defense the defense to set
+	 */
+	public void setDefense(double defense) {
+		this.defense = defense;
+	}
+
+	/**
+	 * @return the perception
+	 */
+	public double getPerception() {
+		return perception;
+	}
+
+	/**
+	 * @param perception the perception to set
+	 */
+	public void setPerception(double perception) {
+		this.perception = perception;
+	}
+
+	/**
+	 * @return the stealth
+	 */
+	public double getStealth() {
+		return stealth;
+	}
+
+	/**
+	 * @param stealth the stealth to set
+	 */
+	public void setStealth(double stealth) {
+		this.stealth = stealth;
+	}
+
+	/**
+	 * @return the totalHealth
+	 */
+	public double getTotalHealth() {
+		return totalHealth;
+	}
+
+	/**
+	 * @param totalHealth the totalHealth to set
+	 */
+	private void setTotalHealth(double totalHealth) {
+		this.totalHealth = totalHealth;
+	}
+
+	/**
+	 * @return the currentHealth
+	 */
+	public double getCurrentHealth() {
+		return currentHealth;
+	}
+
+	/**
+	 * @param currentHealth the currentHealth to set
+	 */
+	public void setCurrentHealth(double currentHealth) {
+		this.currentHealth = currentHealth;
+	}
+
+	/**
+	 * @return the bag
+	 */
+	public List<Item> getBag() {
+		return bag;
+	}
+
+	/**
+	 * @param bag the bag to set
+	 */
+	public void setBag(List<Item> bag) {
+		this.bag = bag;
+	}
+
+	/**
+	 * @param item to add to bag
+	 */
+	public void addItemToBag(Item item) {
+		bag.add(item);
+	}
+
+	/**
+	 * @param item to remove from bag
+	 */
+	public void removeItemFromBag(Item item) {
+		bag.remove(item);
+	}
+
+	/**
+	 * @return the weapon
+	 */
+	public Item getWeapon() {
+		return weapon;
+	}
+
+	/**
+	 * @param weapon the weapon to set
+	 */
+	public void setWeapon(Item weapon) {
+		this.weapon = weapon;
+	}
+}
